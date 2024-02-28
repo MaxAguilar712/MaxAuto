a1b8f54948d4f31eb937237b02bdc52487a652fd
MaxAguilar712 <a26001977@gmail.com>
2024-02-28 18:52:11 -0500

    css n buttons

--- MaxAuto/MaxAuto/client/maxauto/src/components/Cars/Car.js
+++ MaxAuto/MaxAuto/client/maxauto/src/components/Cars/Car.js
@@ -1,23 +1,20 @@
 import React from "react";
-import { Card, CardImg, CardBody } from "reactstrap";
+import { Card, CardImg, CardBody, CardFooter, Button } from "reactstrap";
 import { Link } from "react-router-dom";
 import './Car.css';
 
 export const Car = ({ car }) => {
   return (
-    <Card className='h-10' style={{margin: "2em", width:" 35%", height:"auto", background: "#727272"}}>
-      <p className="text-center" style={{paddingTop: "2vh", fontSize: "2rem", color: "white"}}>
+    <Card className='h-10' style={{margin: "1.5em", width:" 500px", height:"auto", background: "#727272"}}> 
+	     <CardBody >      </CardBody>
+      <CardImg className="Shadow" style={{margin: "2vh"}} top src={car.imageUrl} alt={car.name}  />
+	  <div className="text-center" style={{paddingTop: "1vh", fontSize: "3vh ", color: "white"}}>
         <p className="text-left px-2">{car?.year} {car?.manufacturer} {car?.name}</p>
 		<p className="text-left px-2">Transmission: {car?.transmission}</p>
 		<p className="text-left px-2"> Mileage: {car?.mileage} </p>
-      </p>
-      <CardImg className="Shadow" top src={car.imageUrl} alt={car.name}  />
-      <CardBody >
-         <p>
-          <p style={{color:"white"}}>
-          <strong> ${car.price}.00</strong>
-          </p>
-        </p>
+      </div>
+      
+
         {/* <p>{car.caption}</p> */}
         {/* <p>Comments:</p>
         <div>
@@ -28,7 +25,23 @@ export const Car = ({ car }) => {
                 </>
             ))}
         </div>  */}
-      </CardBody>
+
+	  <CardFooter>  
+		        <p style={{color:"white"}}>
+          		<strong> ${car.price}.00  </strong>
+				  <Button
+				
+					Dark
+					color='Dark'
+					className='me-2 btn-dark'
+					onClick={(e) => {
+						e.preventDefault();
+						// navigate(`/Post/${post.id}/Comments`);
+					}}
+				>
+					Purchase
+				</Button></p>
+			</CardFooter>
     </Card>
   );
 };
